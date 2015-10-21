bd917e8768879d4ac6f4d3048a6c8dbf058df655
Ron <ron_oudgenoeg@hotmail.com>
2015-10-21 09:36:24 +0200

    S60.21 added fixtures

--- Test/Fixture/ExchangeRateLogFixture.php
+++ Test/Fixture/ExchangeRateLogFixture.php
@@ -17,6 +17,14 @@ class ExchangeRateLogFixture extends CakeTestFixture {
  *
  * @var array
  */
-	public $records = array();
+	public $records = array(
+		array(
+			'id' => 1,
+			'message' => 'Currency \'EUR\' has changed more than the alert threshold of 5%. Difference percentage: 5%; Old rate: 1.3; New rate: 2;',
+			'exchange_rate_id' => 64,
+			'created' => '2015-10-20 16:00:04',
+			'modified' => '2015-10-20 16:00:04',
+		)
+	);
 
 }
